e31cdfc2127a919fd30da1518b2ebf13247b11fe
Alex Sanzana <alexsanzana1@gmail.com>
2020-09-16 12:15:26 -0300

    Sección 12 - Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
+const usuario = require('../models/usuario');
 
 const login = async(req, res = response) => {
 
@@ -43,6 +45,54 @@ const login = async(req, res = response) => {
     }
 }
 
+
+const googleSingIn = async(req, res = response) => {
+
+    const googleToken = req.body.token;
+    try {
+
+        const { name, email, picture } = await googleVerify(googleToken);
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if (!usuarioDB) {
+            // esto es si no existe el usuario.
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            });
+        } else {
+            // existe usuario
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        // Guardar en BD
+        await usuario.save();
+
+        // Generar el Token - JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            ok: true,
+            token
+        });
+
+    } catch (error) {
+        res.status(401).json({
+            ok: false,
+            msg: 'Token no es correcto'
+        });
+    }
+
+}
+
+
+
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 require('dotenv').config(); // estoy leyendo un documento con las variables de entorno
 const express = require('express');
 const cors = require('cors');
-
 const { dbConnection } = require('./database/config');
 
 // crear el servidor express
 const app = express();
-
 // Configurar CORS
 app.use(cors());
-
 // Lectura y parseo del body
 app.use(express.json());
-
 // Base de Datos
 dbConnection();
-
+// Directorio Publico... cualquier persona vera el contenido de este directorio publico... se crea el siguiente middleware
+app.use(express.static('public'));
 // Rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/hospitales', require('./routes/hospitales'));
